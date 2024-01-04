29b254a3aee86924b42ade7590eb982c7dcc3b15
Victor Kim <vskimjr@gmail.com>
2024-01-04 15:26:02 -0500

    added route for /:code

--- routes/companies.js
+++ routes/companies.js
@@ -4,7 +4,8 @@ const router = new express.Router();
 const db = require("../db");
 
 // routes go here
-/**Returns a list of companies */
+
+/** Returns a list of companies {companies: [{code, name}, ...]} */
 router.get("/", async function(req, res){
   const result = await db.query(
     `SELECT code, name, description
@@ -13,6 +14,19 @@ router.get("/", async function(req, res){
   const companies = result.rows;
   return res.json({ companies });
 })
+
+/** Returns obj of company {company: {code, name, description}} */
+
+router.get("/:code", async function(req, res){
+  const code = req.params.code;
+  const result = await db.query(
+    `SELECT code, name, description
+          FROM companies
+          WHERE code = $1`, [code]
+  );
+  const company = result.rows[0]
+  return res.json({ company });
+});
 
 
 module.exports = router;
