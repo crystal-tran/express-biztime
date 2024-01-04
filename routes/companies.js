6d95f565d35c3b2977e923c80128a1cd417a1518
Victor Kim <vskimjr@gmail.com>
2024-01-04 15:31:42 -0500

    Started post route for /companies. Need to finish

--- routes/companies.js
+++ routes/companies.js
@@ -1,4 +1,4 @@
-const { ExpressError } = require("../expressError");
+const { ExpressError, BadRequestError } = require("../expressError");
 const express = require("express");
 const router = new express.Router();
 const db = require("../db");
@@ -6,27 +6,42 @@ const db = require("../db");
 // routes go here
 
 /** Returns a list of companies {companies: [{code, name}, ...]} */
-router.get("/", async function(req, res){
+router.get("/", async function (req, res) {
   const result = await db.query(
     `SELECT code, name, description
           FROM companies`
   );
   const companies = result.rows;
   return res.json({ companies });
-})
+});
 
 /** Returns obj of company {company: {code, name, description}} */
 
-router.get("/:code", async function(req, res){
+router.get("/:code", async function (req, res) {
   const code = req.params.code;
   const result = await db.query(
     `SELECT code, name, description
           FROM companies
           WHERE code = $1`, [code]
   );
-  const company = result.rows[0]
+  const company = result.rows[0];
   return res.json({ company });
 });
+
+/**
+ * Adds a company
+ * receives JSON like: {code, name, description}
+ * returns obj of new company: {company: {code, name, description}}
+*/
+
+router.post("/", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+  const result =  await db.query(
+    `INSERT INTO companies (code)
+          VALUES ($1)
+          RETURNING `
+  )
+})
 
 
 module.exports = router;
